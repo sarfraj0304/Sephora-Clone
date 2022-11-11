a4cec77557d7d1286d3e87f9c6a799ca5aeaaf12
Ayush <ayush8583@gmail.com>
2022-11-11 20:08:22 +0530

    fw21_0574_day-4

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,14 +1,15 @@
 let data=JSON.parse(localStorage.getItem("Cart_Product"));
 
+let data_price=JSON.parse(localStorage.getItem("updatePrice"));
 
-
-display(data);
+   display(data)
 
 
 console.log("data",data)
 
-function display(data){
-
+function display(data,updatePrice){
+    document.getElementById("rest_div").innerHTML=null
+    document.getElementById("image_div").innerHTML=null
         let image=document.createElement("img")
         image.src=data.image;
         let brand=document.createElement("h5")
@@ -17,7 +18,7 @@ function display(data){
         btn.innerText="Remove"
         btn.setAttribute("class","btn")
         let price=document.createElement("h4")
-        price.innerText=(+(data.price))
+        price.innerText=("$"+(updatePrice||data.price))
         
       
         
@@ -26,11 +27,12 @@ function display(data){
         }
         
         let show = document.getElementById("quantity");
-show.onclick=()=>{
+show.onchange=()=>{
 
     appendPrice();
     
 }
+
 
 function appendPrice(){
 
@@ -39,5 +41,15 @@ function appendPrice(){
 
 
 
+        let updatePrice=data.price*quantity
+document.getElementById("s_price").innerText= "$"+updatePrice
+document.getElementById("a_price").innerText= "$"+updatePrice
+        display(data,updatePrice)
+        localStorage.setItem("updatePrice",JSON.stringify(updatePrice))
+       
+
 };
+
 document.getElementById("count").innerText=1
+document.getElementById("s_price").innerText= "$"+data.price
+document.getElementById("a_price").innerText= "$"+data.price
